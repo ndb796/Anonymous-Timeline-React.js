9af859e887988eda3b8d295108c38e313f221023
ndb796 <ndb796@naver.com>
2019-05-23 19:45:41 +0900

    Add module that gets board list & searchs board list

--- src/App.js
+++ src/App.js
@@ -1,27 +1,55 @@
 import React from 'react';
 import Board from './components/Board';
 
-const boards = [
-  {
-    id: 1,
-    name: '홍길동',
-    content: '안녕하세요.',
-    date: new Date().toString()
-  },
-  {
-    id: 2,
-    name: '나동빈',
-    content: '반갑습니다.',
-    date: new Date().toString()
-  }
-]
+const databaseURL = "http://localhost:3000/api/board";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      boards: {},
+      searchName: '',
+      searchContent: ''
+    }
+  }
+  _get() {
+    fetch(`${databaseURL}`).then(res => {
+      if(res.status !== 200) {
+          throw new Error(res.statusText);
+      }
+      return res.json();
+    }).then(data => this.setState({boards: data['data']}));
+  }
+  componentDidMount() {
+    this._get();
+  }
+  handleSearch = () => {
+    fetch(`${databaseURL}?name=${this.state.searchName}&content=${this.state.searchContent}`).then(res => {
+      if(res.status !== 200) {
+          throw new Error(res.statusText);
+      }
+      return res.json();
+    }).then(data => this.setState({boards: data['data']}));
+  }
+  handleValueChange = (e) => {
+    let nextState = {};
+    nextState[e.target.name] = e.target.value;
+    this.setState(nextState);
+  }
+  handleEnter = (e) => {
+    if(e.key === 'Enter') {
+      this.handleSearch();
+    }
+  }
   render() {
     return (
       <div>
-        {boards.map(board => {
-          return <Board id={board.id} name={board.name} content={board.content} date={board.date}/>
+        이름: <input type="text" name="searchName" value={this.state.searchName} onChange={this.handleValueChange} onKeyPress={this.handleEnter}/>
+        내용: <input type="text" name="searchContent" value={this.state.searchContent} onChange={this.handleValueChange} onKeyPress={this.handleEnter}/>
+        <input type="button" value="검색" onClick={this.handleSearch}/>
+        {Object.keys(this.state.boards).map(id => {
+          let board = this.state.boards[id];
+          return <Board key={board.id} id={board.id} name={board.name} content={board.content} date={board.date}/>
         })}
       </div>
     );
